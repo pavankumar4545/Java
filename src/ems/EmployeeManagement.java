a5dd9bf25a120b23d830ae8418ee89419fff0481
pavankumar4545 <pavankumarmadli48@gmail.com>
2025-03-04 14:10:48 +0530

    Employee Management System Code

---
+++ src/ems/EmployeeManagement.java
@@ -0,0 +1,42 @@
+package ems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+
+    public void displayDetails() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Salary: $" + salary);
+    }
+}
+
+// Main class
+public class EmployeeManagement {
+    public static void main(String[] args) {
+
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new Employee(101, "Pavankumar Madli", 25000));
+        employees.add(new Employee(102, "Abhishek", 25000));
+        employees.add(new Employee(103, "Veer", 25000));
+
+
+        System.out.println("Employee Details:");
+        for (Employee emp : employees) {
+            emp.displayDetails();
+        }
+    }
+}
